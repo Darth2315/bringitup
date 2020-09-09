a440c559958cb76e12c21dd30d8592a418a24fad
Alex <sasha-a@ukr.net>
2020-09-09 22:27:19 +0300

    All mini sliders is allready done

--- src/js/modules/slider/slider-mini.js
+++ src/js/modules/slider/slider-mini.js
@@ -1,8 +1,59 @@
 import Slider from './slider';
 
 export default class MiniSlider extends Slider {
-    constructor(container, prev, next) {
-        super(container, prev, next);
+    constructor(container, prev, next, activeClass, animate, autoplay) {
+        super(container, prev, next, activeClass, animate, autoplay);
+    }
+
+    decorizeSlides() {
+        this.slides.forEach(item => {
+            item.classList.remove(this.activeClass);
+            if (this.animate) {
+                item.querySelector('.card__title').style.opacity = '0.4';
+                item.querySelector('.card__controls-arrow').style.opacity = '0';
+            }
+        });
+
+        if (!this.slides[0].closest('button')) {
+            this.slides[0].classList.add(this.activeClass);
+        }
+        
+        if (this.animate) {
+            this.slides[0].querySelector('.card__title').style.opacity = '1';
+            this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
+        }
+    }
+
+    nextSlide() {
+        if (this.slides[1].tagName == 'BUTTON' && this.slides[2].tagName == 'BUTTON') {
+            this.container.appendChild(this.slides[0]); // slide
+            this.container.appendChild(this.slides[1]); // btn
+            this.container.appendChild(this.slides[2]); // btn
+            this.decorizeSlides();
+        } else if (this.slides[1].tagName == 'BUTTON') {
+            this.container.appendChild(this.slides[0]); // slide
+            this.container.appendChild(this.slides[1]); // btn
+            this.decorizeSlides();
+        } else {
+            this.container.appendChild(this.slides[0]);
+            this.decorizeSlides();
+        }   
+    }
+
+    bindTriggers() {
+        this.next.addEventListener('click', () => this.nextSlide());
+
+        this.prev.addEventListener('click', () => {
+
+            for (let i = this.slides.length - 1; i > 0; i--) {
+                if (this.slides[i].tagName !== 'BUTTON') {
+                    let active = this.slides[i];
+                    this.container.insertBefore(active, this.slides[0]);
+                    this.decorizeSlides();
+                    break;
+                }
+            }            
+        });
     }
 
     init() {
@@ -12,5 +63,12 @@ export default class MiniSlider extends Slider {
             overflow: hidden;
             align-items: flex-start;
         `;
+
+        this.bindTriggers();
+        this.decorizeSlides();
+
+        if (this.autoplay) {
+            setInterval(() => this.nextSlide(), 5000);
+        }
     }
 }
